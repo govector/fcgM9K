5a380dec5e4c57286d3cc12aa71e380a1a3c8464
Danny L <35980685+govector@users.noreply.github.com>
2018-03-14 01:13:06 -0400

    Update shared.lua

--- ak-47_-_vulcan_417856423/lua/weapons/clt_akvlcn/shared.lua
+++ ak-47_-_vulcan_417856423/lua/weapons/clt_akvlcn/shared.lua
@@ -33,7 +33,7 @@ SWEP.Primary.RPM			= 500
 SWEP.Primary.ClipSize			= 30		
 SWEP.Primary.DefaultClip		= 90		
 SWEP.Primary.KickUp				= 0.6		
-SWEP.Primary.KickDown			= 0.3		
+SWEP.Primary.KickDown			= 0.2		
 SWEP.Primary.KickHorizontal		= 0.4		
 SWEP.Primary.Automatic			= true		
 SWEP.Primary.Ammo			= "ar2"			
@@ -47,8 +47,8 @@ SWEP.data 				= {}
 SWEP.data.ironsights			= 1
 
 SWEP.Primary.Damage		= 45	
-SWEP.Primary.Spread		= .03	
-SWEP.Primary.IronAccuracy = .02 
+SWEP.Primary.Spread		= .015	
+SWEP.Primary.IronAccuracy = .008
 
 SWEP.IronSightsPos = Vector ( 4.869, -14.039, 1.769 )
 SWEP.IronSightsAng = Vector ( 2.342, -0.145, 0 )
