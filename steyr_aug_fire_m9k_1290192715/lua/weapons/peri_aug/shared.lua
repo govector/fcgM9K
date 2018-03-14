7f5e8fe2b0aabfd482850b754e058706ddbe8ac2
Danny L <35980685+govector@users.noreply.github.com>
2018-03-13 20:59:05 -0400

    Update shared.lua

--- steyr_aug_fire_m9k_1290192715/lua/weapons/peri_aug/shared.lua
+++ steyr_aug_fire_m9k_1290192715/lua/weapons/peri_aug/shared.lua
@@ -37,7 +37,7 @@ SWEP.Primary.KickUp				= 0.2		-- Maximum up recoil (rise)
 SWEP.Primary.KickDown			= 0.1		-- Maximum down recoil (skeet)
 SWEP.Primary.KickHorizontal		= 0.3		-- Maximum up recoil (stock)
 SWEP.Primary.Automatic			= true		-- Automatic = true; Semi Auto = false
-SWEP.Primary.Ammo			= "smg1"			-- pistol, 357, smg1, ar2, buckshot, slam, SniperPenetratedRound, AirboatGun
+SWEP.Primary.Ammo			= "ar2"			-- pistol, 357, smg1, ar2, buckshot, slam, SniperPenetratedRound, AirboatGun
 -- Pistol, buckshot, and slam always ricochet. 
 --Use AirboatGun for a light metal peircing shotgun pellets
 
